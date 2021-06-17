7d3bf4b401564e9ae278baf124084edbe6f5d408
pritish19076 <58362873+pritish19076@users.noreply.github.com>
2021-06-17 07:15:24 +0530

    Changes Background According to Temperature

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const body = document.body;
 const myForm = document.getElementById('myForm');
 const API="b257e4b4dfe8bb2ce49be0d46fe1a308";
 const container=document.getElementsByClassName('weather-container')[0];
+const upanim=document.getElementById('animate');
 let showResult;
 let made=false;
 const myFun = async () => {
@@ -14,23 +15,33 @@ const myFun = async () => {
 	const minTemp=Math.floor((main["temp_min"]-273.15)* 100) / 100;
 	const maxTemp=Math.floor((main["temp_max"]-273.15)* 100) / 100;
 	const Temp=Math.floor((main["temp"]-273.15)* 100) / 100;
+	if(maxTemp>30){
+		body.style.backgroundImage = "url(summer.jpg)";
+		body.style.color="white";
+
+	}
+	else if(maxTemp>20){
+		body.style.backgroundImage = "url(autumn.jpg)";
+		body.style.color="white";
+	}
+	else{
+		body.style.backgroundImage = "url(snow.jpg)";
+		body.style.color="black";
+	}
 	const Desc=weather[0]["description"];
 	console.log(minTemp);
 	if(!made){
 		showResult = document.createElement('div');
-		
-		console.log("made")
+		showResult.classList.add('weatherdetails');
 	}
-    showResult.classList.add('weatherdetails');
-	console.log(showResult);
+   
 	showResult.innerHTML=`Temp: ${Temp} ° C <br>Min-Temp: ${minTemp} ° C <br>Max-Temp: ${maxTemp} ° C <br> Description: ${Desc}`
-	//container.classList.add('animateup');
+	
+	
 	if(!made){
 		container.appendChild(showResult);
 		made=true;
 	}
-	
-  	//createBreakingBadCard(BreakingBad[0]);
 	return false;
 
 }
